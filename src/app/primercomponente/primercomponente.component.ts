1c2072778d7c29aad6f35cd78586d04204315e61
Byron-DxO <57022159+Byron-DxO@users.noreply.github.com>
2019-10-26 21:17:18 -0600

    a
    a

---
+++ src/app/primercomponente/primercomponente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'Login',
+   templateUrl: './primercomponente.component.html',
+ 
+    
+    
+})
+export class Login {
+     
+}
